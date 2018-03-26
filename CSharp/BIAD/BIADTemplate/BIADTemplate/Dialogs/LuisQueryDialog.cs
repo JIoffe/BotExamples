81dda3db56b230f5384f5ab9ffaec7e70a665ce0
Jason Ioffe <Jason.Ioffe@insight.com>
2018-03-26 10:29:24 -0400

    Added BIAD template

---
+++ CSharp/BIAD/BIADTemplate/BIADTemplate/Dialogs/LuisQueryDialog.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace BIADTemplate.Dialogs
+{
+    /// <summary>
+    /// Extremely simplistic example of how to use LUIS to route a conversation
+    /// </summary>
+    [Serializable]
+    public class LuisQueryDialog: LuisDialog<string>
+    {
+        public static ILuisService GetLuisService()
+        {
+            var appId = ConfigurationManager.AppSettings["LuisAppId"];
+            var apiKey = ConfigurationManager.AppSettings["LuisAPIKey"];
+            var attribute = new LuisModelAttribute(appId, apiKey);
+
+            return new LuisService(attribute);
+        }
+
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        {
+            context.Done("none");
+        }
+    }
+}
---
+++ CSharp/BIAD/BIADTemplate/BIADTemplate/Dialogs/QnaDialog.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+using System;
+using System.Configuration;
+
+namespace BIADTemplate.Dialogs
+{
+    [Serializable]
+    public class QnaDialog: QnAMakerDialog
+    {
+        public QnaDialog() :
+            base(new QnAMakerService(new QnAMakerAttribute(ConfigurationManager.AppSettings["QnaSubscriptionKey"], ConfigurationManager.AppSettings["QnaKnowledgebaseId"], "Sorry, I couldn't find an answer for that", 0.5)))
+        {
+        }
+    }
+}
