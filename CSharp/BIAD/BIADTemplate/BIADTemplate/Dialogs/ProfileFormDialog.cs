94e2cf02442a175b0f32364ea43e6dc41cc88a13
Jason Ioffe <Jason.Ioffe@insight.com>
2018-03-26 22:45:56 -0400

    Added very simple form builder example

---
+++ CSharp/BIAD/BIADTemplate/BIADTemplate/Dialogs/ProfileFormDialog.cs
@@ -0,0 +1,42 @@
+﻿using BIADTemplate.Model;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Connector;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BIADTemplate.Dialogs
+{
+    [Serializable]
+    public class ProfileFormDialog: IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as IMessageActivity;
+            await context.Forward(FormDialog.FromForm(BuildProfileForm), ResumeAfter_ProfileForm, activity, CancellationToken.None);
+        }
+
+        private async Task ResumeAfter_ProfileForm(IDialogContext context, IAwaitable<Profile> result)
+        {
+            var profile = await result;
+            await context.PostAsync($"Alright, {profile.FirstName} {profile.LastName} the {profile.Occupation} from {profile.HomeTown}. Got it. Welcome!");
+            context.Done(string.Empty);
+        }
+        private static IForm<Profile> BuildProfileForm()
+        {
+            return new FormBuilder<Profile>()
+                .Field(nameof(Profile.FirstName), "What's your first name?")
+                .Field(nameof(Profile.LastName), "Great, and your last name?")
+                .Field(nameof(Profile.Occupation), "...and what do you do for a living??")
+                .Field(nameof(Profile.HomeTown), "Where are you from?")
+                .Build();
+        }
+    }
+}
---
+++ CSharp/BIAD/BIADTemplate/BIADTemplate/Model/Profile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BIADTemplate.Model
+{
+    [Serializable]
+    public class Profile
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Occupation { get; set; }
+        public string HomeTown { get; set; }
+    }
+}
